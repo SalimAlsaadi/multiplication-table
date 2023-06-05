f9baa666df885385fc8515369dc1336a923c9c27
SalimAlsaadi <al-saadis-s-s@hotmail.com>
2023-06-05 07:25:52 -0700

    multiplication table

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Nested_For_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+       for(int a = 1; a<=12; a++)
+            {
+                Console.WriteLine("multiplication table of  " + a + " ");
+                for(int b = 1; b<=12; b++)
+                {
+                    int num = a * b;
+
+                    Console.WriteLine( a + " * "+ b + " = "+ num);
+                    
+                }
+            }
+
+              
+        
+        }
+    }
+}
